e43a3962cf44935e104fad9692cab8897edf0129
ajlopez <webmaster@ajlopez.com>
2015-07-19 09:04:37 -0300

    Create vector with values

---
+++ Src/MathelSharp.Tests/VectorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace MathelSharp.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class VectorTests
+    {
+        [TestMethod]
+        public void CreateWithValues()
+        {
+            Vector vector = new Vector(new double[] { 1.0, 2.0, 3.0 });
+
+            Assert.AreEqual(3, vector.Size);
+
+            var elements = vector.Elements;
+
+            Assert.AreEqual(1.0, elements[0]);
+            Assert.AreEqual(2.0, elements[1]);
+            Assert.AreEqual(3.0, elements[2]);
+        }
+    }
+}
---
+++ Src/MathelSharp/Vector.cs
@@ -0,0 +1,27 @@
+﻿namespace MathelSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Vector
+    {
+        private double[] elements;
+
+        public Vector(IList<double> elements)
+        {
+            this.elements = elements.ToArray();
+        }
+
+        public int Size { get { return this.elements.Length; } }
+
+        public IList<double> Elements
+        {
+            get
+            {
+                return new List<double>(this.elements);
+            }
+        }
+    }
+}
