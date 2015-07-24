1eaddb088ed07a9837d542f82f53ec13ece8c929
ajlopez <webmaster@ajlopez.com>
2015-07-24 07:11:13 -0300

    Create matrix first test

---
+++ Src/MathelSharp.Tests/MatrixTests.cs
@@ -0,0 +1,27 @@
+﻿namespace MathelSharp.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MatrixTests
+    {
+        [TestMethod]
+        public void CreateWithValues()
+        {
+            Matrix matrix = new Matrix(new double[][] { new double[] { 1.0, 2.0 } , new double[] { 3.0, 4.0 } });
+
+            Assert.AreEqual(4, matrix.Size);
+
+            var elements = matrix.Elements;
+
+            Assert.AreEqual(1.0, elements[0][0]);
+            Assert.AreEqual(2.0, elements[0][1]);
+            Assert.AreEqual(3.0, elements[1][0]);
+            Assert.AreEqual(4.0, elements[1][1]);
+        }
+    }
+}
---
+++ Src/MathelSharp/Matrix.cs
@@ -0,0 +1,24 @@
+﻿namespace MathelSharp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Matrix
+    {
+        private double[][] elements;
+
+        public Matrix(IList<IList<double>> elements)
+        {
+            this.elements = new double[elements.Count][];
+
+            for (int k = 0; k < elements.Count; k++)
+                this.elements[k] = elements[k].ToArray();
+        }
+
+        public int Size { get { return this.elements.Length * this.elements[0].Length; } }
+
+        public double[][] Elements { get { return this.elements; } }
+    }
+}
